7909d406c8aa91271352cbe45957dacf664cd096
bhanu <bhanuputtareddy@gmail.com>
2018-05-26 17:51:02 +0530

    cleaned code

--- Cricbuzz_api.py
+++ Cricbuzz_api.py
@@ -40,53 +40,57 @@ completed_feed = ""
 # dictionary is useful when integrated with bots/messages/mails
 dict = {'live_matches': live_feed, 'upcoming_matches': upcoming_feed, 'completed_matches': completed_feed}
 
-# Iterating through all matches available
-for match in root.findall('match'):
-    series = match.find('srs')
-    state = match.find('state')
-    match_state = match.find('state')
-    match_details = match.attrib
-    status_details = state.attrib
-    # print match.attrib
-    # print state.attrib
-    # print match_details["srs"]
-    match_status = status_details["mchState"]
-    # print match_status
 
-    if(match_status == MATCH_STATUS_UPCOMING):
-        upcoming_matches = UPCOMING_MATCH_DETAILS_HEADER
-        upcoming_matches = upcoming_matches + match_details["srs"] + "\n"
-        upcoming_matches = upcoming_matches + match_details["mchDesc"] + " in " + match_details["grnd"]+"\n"
-        upcoming_matches = upcoming_matches + "will be playing "+match_details["mnum"] + "\n"
-        upcoming_matches = upcoming_matches + status_details["status"] + "\n"
-        upcoming_feed = upcoming_feed + upcoming_matches
-        dict['upcoming_matches'] = upcoming_feed
+def main():
+    # Iterating through all matches available
+    for match in root.findall('match'):
+        series = match.find('srs')
+        state = match.find('state')
+        match_state = match.find('state')
+        match_details = match.attrib
+        status_details = state.attrib
+        # print match.attrib
+        # print state.attrib
+        # print match_details["srs"]
+        match_status = status_details["mchState"]
+        # print match_status
 
-    elif(match_status == MATCH_STATUS_LIVE):
-        live_matches = LIVE_MATCH_DETAILS_HEADER
-        match_score = match.find("mscr")
-        bt_tm = match_score.find("btTm")
-        inngs = bt_tm.find("Inngs")
-        live_matches = live_matches + bt_tm["sName"] + ": " + inngs["r"] + "for " + inngs["wkts"] + " in "+ inngs["ovrs"] + "\n"
-        live_matches = live_matches + "playing in "+match_details["mnum"] + "\n"
-        live_feed = live_feed + live_matches
-        dict['live_feed'] = live_feed
+        if match_status == MATCH_STATUS_UPCOMING:
+            upcoming_matches = UPCOMING_MATCH_DETAILS_HEADER
+            upcoming_matches = upcoming_matches + match_details["srs"] + "\n"
+            upcoming_matches = upcoming_matches + match_details["mchDesc"] + " in " + match_details["grnd"]+"\n"
+            upcoming_matches = upcoming_matches + "will be playing "+match_details["mnum"] + "\n"
+            upcoming_matches = upcoming_matches + status_details["status"] + "\n"
+            upcoming_feed = upcoming_feed + upcoming_matches
+            dict['upcoming_matches'] = upcoming_feed
 
-    elif(match_status in COMPLETED_MATCH_STATES):
-        completed_matches = COMPLETED_MATCH_DETAILS_HEADER
-        completed_matches = completed_matches + match_details["srs"] + "\n"
-        completed_matches = completed_matches + match_details["mchDesc"] + "\n"
-        completed_matches = completed_matches + status_details["status"] + "\n"
-        completed_matches = completed_matches + "has played in " + match_details["mnum"] + "\n"
-        completed_feed = completed_feed + completed_matches
-        dict['completed_matches'] = completed_feed
+        elif match_status == MATCH_STATUS_LIVE:
+            live_matches = LIVE_MATCH_DETAILS_HEADER
+            match_score = match.find("mscr")
+            bt_tm = match_score.find("btTm")
+            inngs = bt_tm.find("Inngs")
+            live_matches = live_matches + bt_tm["sName"] + ": " + inngs["r"] + "for " + inngs["wkts"] + " in "+ inngs["ovrs"] + "\n"
+            live_matches = live_matches + "playing in "+match_details["mnum"] + "\n"
+            live_feed = live_feed + live_matches
+            dict['live_feed'] = live_feed
 
-# printing to console for testing
-print dict['live_matches']
-print dict['upcoming_matches']
-print dict['completed_matches']
+        elif match_status in COMPLETED_MATCH_STATES:
+            completed_matches = COMPLETED_MATCH_DETAILS_HEADER
+            completed_matches = completed_matches + match_details["srs"] + "\n"
+            completed_matches = completed_matches + match_details["mchDesc"] + "\n"
+            completed_matches = completed_matches + status_details["status"] + "\n"
+            completed_matches = completed_matches + "has played in " + match_details["mnum"] + "\n"
+            completed_feed = completed_feed + completed_matches
+            dict['completed_matches'] = completed_feed
+
+    # printing to console for testing
+    print dict['live_matches']
+    print dict['upcoming_matches']
+    print dict['completed_matches']
 
 
+if __name__ == '__main__':
+    main()
 
 
 
