5aac47c41de40c953f6d9098dc48b340a3c8313b
Bhanu Reddy <bhanuputtareddy@gmail.com>
2018-11-06 22:21:35 +0530

    determine local machine ipaddress in the network

---
+++ network_scanner.py
@@ -0,0 +1,16 @@
+import socket
+
+'''
+Determine your own IP address
+Determine your own netmask
+Determine the network range
+Scan all the addresses (except the lowest, which is your network address and the highest, which is your broadcast address).
+Use your DNS's reverse lookup to determine the hostname for IP addresses which respond to your scan.
+'''
+#determine local machine ip address
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+local_ip = s.getsockname()[0]
+s.close()
+
+#determine netmask
