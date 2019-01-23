c615d5dadf571207a6e4582cf4fbc9e5222fb033
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-01-23 16:21:44 +0530

    updated for windows10

--- wallpaper_change.py
+++ wallpaper_change.py
@@ -1,4 +1,6 @@
 import os
+import struct
+import ctypes
 import random
 import logging
 import platform
@@ -40,5 +42,15 @@ wallpaper_num = random.randint(0, (max_len-1))
 logger.info("Random number is "+str(wallpaper_num))
 logger.info("wallpaper selected is "+wallpapers_list[wallpaper_num])
 
+
 # for running Ubuntu 18.04 gsettings has to be changed to set wallpaper
+if platform.system()=='linux':
 os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri "+wallpapers_location+"/"+wallpapers_list[wallpaper_num])
+
+#for windows to set wallpaper
+if platform.system()=='Windows':
+    TOTAL=wallpapers_location+"/"+wallpapers_list[wallpaper_num]                   
+    PATH =bytes(TOTAL,'utf-8')
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, PATH, 3)
+
